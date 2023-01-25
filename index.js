6fd9c725feefb92cd622466cea90de81316569de
Jack Lio <jackliomusic@gmail.com>
2023-01-25 19:38:12 +0000

    feat: Added mongo db schemas; added test api endpoint;

--- index.js
+++ index.js
@@ -2,12 +2,46 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose')
+const nanoid = require ('nanoid')
+const bp = require('body-parser');
+
+const nid = nanoid.nanoid();
+
+mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+  console.log('database connected.')
+}).catch((err) => console.log(err.message));
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+  userName: {type: String, required: true},
+  authKey: {type: String, required: true}
+});
+const exSchema = new Schema ({
+  _id: {type: String, required: true},
+  desc: {type: String, required: true},
+  dura: {type: Number, required: true},
+  date: {type: String, required: true}
+});
+
+const User = mongoose.model('User', userSchema);
+const Exercise = mongoose.model('Exercise', exSchema);
 
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+app.use("/api/users", bp.urlencoded({extended: false}));
+app.use(bp.json());
+
+//test first API endpoints
+app.get('/api/hello', (req, res) => {
+  res.send('Greetings: Hello and Welcome!');
+});
+
 
 
 
